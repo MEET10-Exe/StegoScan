1ffed8f740f3cc28b58a7f044bb8ab76ffdbc650
Meet <vaghelameet9090@gamil.com>
2026-06-25 15:07:08 +0530

    fix render port binding

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template, request
 from detector import analyze_image
 from datetime import datetime
 import os
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 scan_count = 0
@@ -61,7 +56,8 @@ def scan():
     return render_template("index.html")
 
 if __name__ == "__main__":
-
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
